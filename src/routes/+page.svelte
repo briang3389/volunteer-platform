7a919b5ee2b5c35e55a16d2350aa6667188a2c53
jack <jack.x.roscoe@gmail.com>
2024-01-21 00:52:04 -0500

    aded new button to create event on homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,16 @@
 <script>
   import { goto } from '$app/navigation';
-  import { userSession } from '$lib/sessionStore';
-  import { onDestroy } from 'svelte';
   import TopBar  from '$lib/TopBar.svelte';
+  import { getLoginData } from "$lib/client_get_loggin_data";
+
+  export let data;
+
+  const loginData = getLoginData(data.token);
 
   //let username = '';
   //const unsubscribe = userSession.subscribe($userSession => {
   //  username = $userSession ? $userSession.username : '';
   //});
-
-  onDestroy(() => {
-    //unsubscribe();
-  });
 
   function navigateToSearch() {
     goto('/search'); // Replace '/search' with the actual path for location search
@@ -24,9 +23,15 @@
     <div class="text-center">
       <h2 class="text-4xl font-bold text-gray-900 mb-4">Helping Communities, One Hour at a Time</h2>
       <img class="block mx-auto max-w-lg h-auto rounded-lg shadow-md mb-4" src="https://students.1fbusa.com/hubfs/25%20Ways%20to%20Volunteer%20in%20Your%20Community.jpg" alt="Volunteers">
-      <button class="bg-blue-600 hover:bg-blue-800 text-white font-bold py-2 px-6 rounded" on:click={navigateToSearch}>
-        Find out What's Going in Your Area
-      </button>
+      {#if loginData?.type === "org"}
+        <button class="bg-blue-600 hover:bg-blue-800 text-white font-bold py-2 px-6 rounded" on:click={() => goto("/create-event")}>
+          Set up an Event in Your Area
+        </button>
+      {:else}
+        <button class="bg-blue-600 hover:bg-blue-800 text-white font-bold py-2 px-6 rounded" on:click={navigateToSearch}>
+          Find out What's Going in Your Area
+        </button>
+      {/if}
     </div>
   </div>
 </div>
