85d43f998c6d0fb658df1f073b71ff67a8ae960b
jack <jack.x.roscoe@gmail.com>
2024-01-20 23:55:32 -0500

    linked event page with backend

---
+++ src/lib/get_queries.ts
@@ -0,0 +1,32 @@
+import { getDb } from "$lib";
+
+export interface Event {
+    name: string,
+    description: string,
+    startdate: string,
+    enddate: string,
+    location: string,
+    orgid: string,
+    icon_url: string,
+}
+
+export async function getEvent(eventid: number): Promise<Event | null> {
+    const db = await getDb();
+    const query = {
+		name: 'get-event',
+		text: 'SELECT name, description, startdate, enddate, location, orgid, icon_url FROM Events WHERE eventid = $1;',
+		values: [eventid],
+	}
+
+	try {
+		const res = (await db.query(query)).rows;
+        if (res.length == 1) {
+            return res[0];
+        }
+	}
+	catch (e) {
+		return null;
+	}
+
+    return null;
+}
---
+++ src/routes/events/[eventId]/+page.server.ts
@@ -0,0 +1,10 @@
+import { getEvent } from "$lib/get_queries";
+
+export async function load({ params }) {
+    const eventid = Number(params.eventId);
+
+    return {
+        eventid: eventid,
+        eventData: await getEvent(eventid),
+    }
+}
