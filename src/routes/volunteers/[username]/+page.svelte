6976ca772a10e56c88482296e7ed0003914887df
CygnusX-26 <qh4nveh@gmail.com>
2024-01-20 20:04:51 -0500

    volunteer page backend integration

--- src/routes/volunteers/[username]/+page.svelte
+++ src/routes/volunteers/[username]/+page.svelte
@@ -1,61 +1,59 @@
 <script lang="ts">
     import TopBar from '$lib/TopBar.svelte'
-	import VolunteerEventList from '$lib/VolunteerEventList.svelte';
+	  import VolunteerEventList from '$lib/VolunteerEventList.svelte';
     import KeywordSearchBar from '$lib/KeywordSearchBar.svelte';
+    import { page } from '$app/stores';
 
-    async function getHours() {
+    export let data;
+
+    async function getInfo() {
         const response = await fetch('/api/get/user', {
             method: 'POST',
-            credentials: 'include',
             headers: {
                 'content-type': 'application/json',
             },
+            body: JSON.stringify({username: data.username})
         });
         if (response.ok) {
             const data = await response.json();
-            return data;
+            console.log(data);
+            return {success: true, info: data};
         } else {
-            console.log('Error fetching events');
+            return {success: false, info: []}
         }
     }
-
-    const info = {
-        totalHours: 5,
-        totalOrgs: 2,
-        imageUrl: "/default_profile_pic.jpg",
-        name: "Nick Andry",
-        email: "me@me.com",
-    }
-
+    
 </script>
 
 <TopBar/>
 
-<div class="search-container">
-    <KeywordSearchBar/>
-</div>
 <div class="container">
     <div class="profile-container">
         <!-- svelte-ignore a11y-img-redundant-alt -->
-        <img src={info.imageUrl} alt="Profile Picture" class="profile-picture">
-        <p class="profile-name">{info.name}</p>
-    </div>
+        {#await getInfo()}
+          <p>...waiting</p>
+        {:then { success, info }}
+        {#if success}
+            {#if info.data.length !== 0}
+                <div class="info-container">
+                <img src={info.data[0].icon_url} alt="" class="profile-picture">
+                <p class="profile-name">{data.username}</p>  
+                <VolunteerEventList userid={info.data[0].userid}/>
 
-    <div class="info-container">
-
-        <div class="totals-container">
-            <div class="totals">
-                <p>Total Hours Volunteered: {info.totalHours}</p>
-                <p>Total Organizations Helped: {info.totalOrgs}</p>
-            </div>
-        </div>
-
-        <VolunteerEventList/>
-
-        <div class="contact-container">
-            <p class="contact-header">Contact Information</p>
-            <p class="profile-email-header">Email: <span class="profile-email">{info.email}</span></p>
-        </div>
+                <div class="contact-container">
+                    <p class="contact-header">Contact Information</p>
+                    <p class="profile-email-header">Email: <span class="profile-email">{info.data[0].email}</span></p>
+                </div>
+                </div>
+            {:else}
+            <p>Failed to fetch user information</p>
+            {/if}
+        {:else}
+        <p>Failed to fetch user information</p>
+      {/if}
+        {:catch error}
+        <p style="color: red">{error.message}</p>
+        {/await}
     </div>
 </div>
 
@@ -88,21 +86,6 @@
   .info-container {
     padding-top: 2rem;
     padding-left: 1rem;
-  }
-
-  .search-container {
-    float:right;
-    padding-right: 3rem;
-  }
-
-  .totals-container {
-    text-align: left;
-    margin-bottom: 20px;
-  }
-
-  .totals {
-    font-size: 2rem;
-    font-weight: bold;
   }
 
   .contact-header {
